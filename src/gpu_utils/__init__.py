167a1b8a6b6501a5138293580cab0bf37b94a922
digitake <digitake@gmail.com>
2021-07-08 12:24:59 +0700

    Update show GPU Code

--- src/gpu_utils/__init__.py
+++ src/gpu_utils/__init__.py
@@ -1,8 +1,9 @@
+from subprocess import check_output
 
 
 def show_gpu():
-  gpu_info = !nvidia-smi
-  gpu_info = '\n'.join(gpu_info)
+  gpu_info = check_output(['nvidia-smi']).decode()
+  #gpu_info = '\n'.join(gpu_info)
   if gpu_info.find('failed') >= 0:
     print('Select the Runtime > "Change runtime type" menu to enable a GPU accelerator, ')
     print('and then re-execute this cell.')
