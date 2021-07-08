05756a0e305bcc18075a4ad8ad3ba10297302c07
digitake <digitake@gmail.com>
2021-07-08 09:51:36 +0700

    Update setup for pip

---
+++ src/gpu_utils/__init__.py
@@ -0,0 +1,10 @@
+
+
+def show_gpu():
+  gpu_info = !nvidia-smi
+  gpu_info = '\n'.join(gpu_info)
+  if gpu_info.find('failed') >= 0:
+    print('Select the Runtime > "Change runtime type" menu to enable a GPU accelerator, ')
+    print('and then re-execute this cell.')
+  else:
+    print(gpu_info)
