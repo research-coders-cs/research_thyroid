e732437afafdf26c5e19dfe3f910a3fd3143fe0a
digitake <digitake@gmail.com>
2021-07-16 08:37:41 +0700

    Add build_train_validation_set to preprocess and bumpversion to 0.4.0

--- src/digitake/preprocess/__init__.py
+++ src/digitake/preprocess/__init__.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import os
 from PIL import Image
 import torchvision.transforms as transforms
-import torch
+import glob
 
 # imagenet mean and std
 imagenet_mean = [0.485, 0.456, 0.406]
@@ -44,3 +44,43 @@ def get_transform(target_size, phase='train'):
         return transform_dict[phase]
     else:
         raise "Unknown pharse specified"
+
+
+def build_train_validation_set(datasource, val_size, root="", ext="*.png"):
+    """
+        datasource: dictionary with key as label of data, and value is a list of image path
+        val_size: validation size, must be greater than total datasource size of each class's dataset
+    """
+    datasets = {}
+    total_dataset = 0
+    for key in datasource:
+        datasets[key] = glob.glob(os.path.join(root, datasource[key], ext))
+        path_count = len(datasets[key])
+        total_dataset += path_count
+        print(f'Found total {path_count} for {key}')
+        assert val_size < path_count, f'The dataset {key} is smaller than validation size'
+
+    training_set = {}
+    validation_set = {}
+    total_training_set = 0
+    total_validation_set = 0
+
+    print("--" * 25)
+
+    for key in datasets:
+        ds_size = len(datasets[key])  # size of each dataset
+
+        training_set[key] = datasets[key][val_size:]  # Cut from val_size-th onward
+        t_size = len(training_set[key])
+        total_training_set += t_size
+        print(f'Total train {key} size = {t_size}/{ds_size} ({t_size / ds_size:0.2f})')
+
+        validation_set[key] = datasets[key][:val_size]  # Validation data is the first n elements from the dataset
+        v_size = len(validation_set[key])
+        total_validation_set += v_size
+        print(f'Total validation {key} size = {v_size}/{ds_size} ({v_size / ds_size:0.2f})')
+
+    print("--" * 25)
+    print(f"Total dataset size = {total_dataset}")
+
+    return training_set, validation_set
