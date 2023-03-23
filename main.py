eda26081a18a0ea69259124ab8ae317ede9cad34
j-devel <38098238+j-devel@users.noreply.github.com>
2023-03-23 17:19:33 +0700

    Set `total_epochs = 100`

--- main.py
+++ main.py
@@ -137,7 +137,8 @@ def _demo_thyroid_train(with_doppler, savepath):
     start_epoch = 0
     #total_epochs = 5
     # total_epochs = 10  # @@
-    total_epochs = 40  # @@
+    # total_epochs = 40  # @@
+    total_epochs = 100  # @@
 
     run_name = f"{model}_{target_resize}_{batch_size}_{lr_}_n{number}"
     print('@@ run_name:', run_name)
