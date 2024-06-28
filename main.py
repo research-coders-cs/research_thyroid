f59c67a050e675e62571a1dc929f52bbbe9c0513
j-devel <38098238+j-devel@users.noreply.github.com>
2024-06-28 11:43:55 +0700

    Add `Encoder`, `Decoder`, `example_mask()` part

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 #from wsdan.digitake.preprocess import build_dataset
 #----
 import transduction  # 'research-ai-transduction' pkg (i.e. 'src/transduction/*')
+
+from transduction import example_mask  # !!!!
+
 from transduction.helper import show_example
 from transduction.vocab import load_tokenizers, load_vocab  # ok with torch 1.11 (enforced by `torchdata==0.3.0`); err with torch 2.3.0
 #----
@@ -19,7 +22,11 @@ if __name__ == '__main__':
 
     print("hello")
 
-    if 1:
+    if 1:  # Encoder/Decoder part
+        show_example(example_mask)
+
+
+    if 0:
         # global variables used later in the script
         spacy_de, spacy_en = show_example(load_tokenizers)
         vocab_src, vocab_tgt = show_example(load_vocab, args=[spacy_de, spacy_en])
