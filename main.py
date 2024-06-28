327f643b0ada56d54e8c266a35b3b3af2a18a94b
j-devel <38098238+j-devel@users.noreply.github.com>
2024-06-28 12:00:05 +0700

    Up to output: "!!!! showme alt.Chart(...)"

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-
-#----
-#import wsdan  # 'research-thyroid-wsdan' pkg (i.e. 'src/wsdan/*')
-#from wsdan.demo import test as demo_test
-#from wsdan.demo import train as demo_train
-#from wsdan.demo import train_with_doppler as demo_train_with_doppler
-#from wsdan.digitake.preprocess import build_dataset
-#----
 import transduction  # 'research-ai-transduction' pkg (i.e. 'src/transduction/*')
 
 from transduction import example_mask  # !!!!
 
 from transduction.helper import show_example
 from transduction.vocab import load_tokenizers, load_vocab  # ok with torch 1.11 (enforced by `torchdata==0.3.0`); err with torch 2.3.0
-#----
 
 import logging
 logger = logging.getLogger('@@')
 logger.setLevel(level=logging.DEBUG if 1 else logging.INFO)
 
+# `mr` when 'src/transduction' is intact
+# `mt` when 'src/transduction' is modified
+
 if __name__ == '__main__':
 
-    print("hello")
+    print("__main__: ^^")
 
     if 1:  # Encoder/Decoder part
-        show_example(example_mask)
+        print("!!!! showme", example_mask())
 
 
     if 0:
         # global variables used later in the script
         spacy_de, spacy_en = show_example(load_tokenizers)
         vocab_src, vocab_tgt = show_example(load_vocab, args=[spacy_de, spacy_en])
-
